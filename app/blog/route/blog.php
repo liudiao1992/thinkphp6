a976ea2d7dd01a153893f64fe8aeebf66d21e644
liudiao <18779469643>
2023-07-25 14:47:13 +0800

    multi app

---
+++ app/blog/route/blog.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @author: liudiao
+ * @Date: 2023-07-25 14:42:33
+ * @Description: blog 路由
+ */
+
+use think\facade\Route;
+
+ Route::group(':version.index', function(){
+    Route::get('hello','h5/:version/index/hello')->name('blog_index_hello');
+ });
